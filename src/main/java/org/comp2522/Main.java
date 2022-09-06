bae853f301b4d0957801f17f50cd9563c5a33edd
Linh Nguyen <k.linh2210@gmail.com>
2022-09-06 12:58:48 -0700

    update new greeting

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -2,6 +2,6 @@ package org.comp2522;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, my name is Linh Nguyen. I am a big fan of dogs and guinea pigs!");
     }
 }
